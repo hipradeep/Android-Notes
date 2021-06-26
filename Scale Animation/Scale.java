2253a913a4fef01d8d5fd64c437794ee5bfdd4d5
Pradeep <32573663+androidbrain@users.noreply.github.com>
2021-06-26 11:16:59 +0530

    Update Scale.java

--- Scale Animation/Scale.java
+++ Scale Animation/Scale.java
@@ -1,6 +1,16 @@
+ <!--Scale Animation, Pass View -->
+  setScaleOnButton(view);
+
+
+
 <!--Scale Animation, put it in Base Activity-->
  
 public void setScaleOnButton(View view){
         Animation scaleAnim = AnimationUtils.loadAnimation(context, R.anim.scale_anim);
         view.startAnimation(scaleAnim);
     }
+
+
+
+
+

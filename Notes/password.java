88aad8b6ea5d9d94e9dd663c595a0819a94cfc80
Pradeep <32573663+hipradeep@users.noreply.github.com>
2021-12-21 12:54:24 +0530

    Create password.java

---
+++ Notes/password.java
@@ -0,0 +1,16 @@
+
+        btn_showPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(!isPasswordVisible) {
+                    et_password.setTransformationMethod(null);
+                    btn_showPassword.setImageResource(R.drawable.eye_black);
+                    isPasswordVisible = true;
+                } else {
+                    et_password.setTransformationMethod(new PasswordTransformationMethod());
+                    btn_showPassword.setImageResource(R.drawable.eye_grey);
+                    isPasswordVisible = false;
+                }
+            }
+        });
